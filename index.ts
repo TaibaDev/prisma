404f5bf3082788a22b28735e6016f897c4c9ceb0
AWAIS-514 (tb) <meet2freelancing@gmail.com>
2024-09-10 19:18:53 +0500

    creating and fetching

--- index.ts
+++ index.ts
@@ -2,13 +2,33 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 async function main(){
-    const createUser = await prisma.user.create({
-        data: {
-            name: "Tayyab Sadiq",
-            email: "tayyab@gmail.com",
-    }})
-    console.log(createUser)
+    // const createUser = await prisma.user.create({
+    //     data: {
+    //         name: "Tariq Sadiq",
+    //         email: "tariq@gmail.com",
+    // }})
+    // console.log(createUser)
+    // const createArticle = await prisma.article.create({
+    //     data: {
+    //         title: "Hello World",
+    //         body: "This is my first article",
+    //         author: {
+    //             connect: {
+    //                 id: 1,
+    //             }
+    //         }
+    //     }
+    // })
+    // console.log(createArticle)
+
+    const getArticles= await prisma.article.findMany({})
+    console.log("getArticles", getArticles)
+    // const getAllUser= await prisma.user.findMany();
+    // console.log("getAllUser", getAllUser)
 }
 
-console.log("updated code");
-// main().then(()=>{}).catch(()=>{});
+main().then(async()=>{await prisma.$disconnect()}).catch(async(e)=>{
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1);
+});
